f8cdfeca94ea5c75061ddf256566d74566864ec6
JONATHAN W. CLINE <JC162499@students.lcisd.org>
2020-01-15 12:44:13 -0600

    Kernel and Drivers
    Finished updating the GPIO driver to the new implemenation style and laid the foundation of the intial kernel code.

--- modules/kernel/kernel7/kernel7.s
+++ modules/kernel/kernel7/kernel7.s
@@ -21,18 +21,47 @@ _abort_data:                b final_
 _irq:                       b final_
 _fiq:                       b final_
 
+/*
+    A small set of variables used for the kernel's bootloader stage
+*/
 
+DT_START:       .word 0x00000000    // start address of the DT block
+DRIVER_STATE:   .word 0x00000000    // bit high when loaded, 0 when not. bit # corresponds to position in below table:
+D_ARM_TIMER:    .word 0x00000000    // 0
+D_BSC_START:    .word 0x00000000    // 1
+D_DMA_START:    .word 0x00000000    // 2
+D_EMMC_START:   .word 0x00000000    // .....
+D_GPIO_START:   .word 0x00000000
+D_PCM_START:    .word 0x00000000
+D_MEM_START:    .word 0x00000000
+D_PWM_START:    .word 0x00000000
+D_SYS_TIMER:    .word 0x00000000
+D_SPI_START:    .word 0x00000000
+D_UART_START:   .word 0x00000000
+D_USB_START:    .word 0x00000000
+
+
+// Code enters here, run the boot load stage
 startup_:
     
-    ldr r0, =0x3F200000
+    // r3 is our current working address pointer and holds the memory value of the DVT we are working with
+    // we will use the stack pointer to load up the drivers and such following the end of the DVT
+    // the value table defined above this routine is what needs to be worked the most. Kernel will have barebones 
+    // functionality for IO with the SD card for loading in these drivers but that is all.
 
-    mov r1, #1
-    lsl r1, r1, #27
-    str r1, [r0, #0x8]
+    // set SP to after the block and save the start address
+    ldr r3, DT_START
+    mov sp, r3
+    ldr r2, [r3, #0x4]
+    add sp, sp, r2
 
-    mov r1, #1
-    lsl r1, r1, #29
-    str r1, [r0, #0x1C]
+    // start reading the DT block
+    add r3, r3, #0x8
+
+
+
 
     final_:
     b final_
+
+
