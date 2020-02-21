aa9b8dd8ce9e57f6552a15d9502577d3393a240b
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2020-02-21 00:41:41 -0600

    Utilities and Some Integration
    Finished up the core utility macros I wanted to add and put together test cases and an actual use case of said macros. These should aid development immensely.

--- modules/asm_utils.s
+++ modules/asm_utils.s
@@ -1,10 +1,14 @@
 /*
-    Regularly used programming utilities
+    Jonathan Cline
+    Regularly used programming utilities put together with macros to ease the burden of more complex programming tasks. 
+    Most of the utilities provided are memory related.
 */
 
 
 /*
+
     STACK - creates a region of memory only accessible through PUSH and POP instructions
+
 */
 
 // Creates a stack inplace
@@ -13,61 +17,79 @@
 // Outputs:
 //      none
 .macro __STACK__ stack_name:req, size:req
-    .equ __STACK_\stack_name\(), .
+    __STACK_POINTER_\stack_name\(): .word . + 0x4
+    __STACK_\stack_name\():
     .org . + (\size * 4)
 .endm
 
-// Pushes a single DWORD onto the stack at the stack pointer given
-// Inputs:
-//      r4 = data to push
-// Ouputs:
-//      none
-.macro __STACK_PUSH__ stack_name:req
+
+// Swaps the current SP with the SP of the referenced stack. Should be used prior and after multiple Q operations for maximum effectiveness
+// INPUTS:
+//      (macro_arg) stack_name = name of the stack this is referencing
+// OUTPUTS:
+//      sp = value of the stack pointer at 
+.macro __STACK_SWAP_SP__ stack_name:req
+    mov r0, sp
+    ldr sp, __STACK_POINTER_\stack_name\()
+    str r0, __STACK_POINTER_\stack_name\()
 .endm
 
-// POPs a single DWORD off of the stack at the stack pointer given
+
+// Pushes one or more registers onto the stack given. This will maintain the current SP value but is slow.
+// Inputs:
+//      (macro_arg) stack_name = name of the stack this is pushing to
+//      (macro_arg) reglist = one or more registers to use, these are added by additional arguements each with a register name. pushed left to right
+// Ouputs:
+//      none
+.macro __STACK_PUSH__ stack_name:req, reglist:vararg
+    __STACK_PUSH_SP_STORE_\@: .word 0x00000000
+    str sp, __STACK_PUSH_SP_STORE_\@
+    ldr sp, __STACK_POINTER_\stack_name\()
+    .irp reg_p, \reglist
+        str \reg_p\(), [sp], #0x4
+    .endr
+    str sp, __STACK_POINTER_\stack_name\()
+    ldr sp, __STACK_PUSH_SP_STORE_\@
+.endm
+
+// Same as above but does not offer safety for the SP value, make sure you are using the correct value!
+.macro __STACK_QPUSH__ stack_name:req, reglist:vararg
+    .irp reg_p, \reglist
+        str \reg_p\(), [sp], #0x4
+    .endr
+.endm
+
+
+// POPs a single DWORD off of the stack at the stack given. This will maintain the current SP value but is slow.
 // Inputs: 
-//      none
+//      (macro_arg) stack_name = name of the stack this is referencing
+//      (marco_arg) reglist = 
 // Ouputs:
-//      r8 = popped value
-.macro __STACK_POP__ stack_name:req
+//      / outputs are dependent on reglist /
+.macro __STACK_POP__ stack_name:req, reglist:vararg
+    __STACK_POP_SP_STORE_\@: .word 0x00000000
+    str sp, __STACK_POP_SP_STORE_\@
+    ldr sp, __STACK_POINTER_\stack_name\()
+    .irp reg_p, \reglist
+        ldr \reg_p\(), [sp, #-0x4]!
+    .endr
+    str sp, __STACK_POINTER_\stack_name\()
+    ldr sp, __STACK_POP_SP_STORE_\@
+.endm
+
+// Same as above but does not offer safety for the SP value, make sure you are using the correct value!
+.macro __STACK_QPOP__ stack_name:req, reglist:vararg
+    .irp reg_p, \reglist
+        ldr \reg_p\(), [sp, #-0x4]!
+    .endr
 .endm
 
 
 /*
-    HEAP - allows for allocation of data only accesible using a direct index value
-*/
 
-// Creates a new heap. This is for all intents and purposes a local heap, but could be used to create a global one.
-// Inputs:
-//      (macro_arg) name = name of this heap for accessing purposes
-// Outputs:
-//      (macro_arg) size = size of the heap in blocks of 32  4 byte wide words. ie: size = 1 allocates 128 bytes of space
-.macro __HEAP__ name:req, size:req
-    .equ __HEAP_TABLE_\name\(), .   // data table to simplify scanning of heap for available space to allocate
-    .org . + (\size * 4)
-    .equ __HEAP_\name\(), .         // actual heap starts here
-    .org . + (\size * 128)
-.endm
-
-// Allocates memory on the heap and returns pointer to start of range
-// Inputs:
-//      heap_name = name of the heap to allocate memory on
-// Outputs:
-//      r8 = address of start of newly allocated memory
-.macro __HEAP_ALLOCATE__ heap_name:req, dword_count:req
-
-    // Grab heap start address and prepare for search
-
-    
-
-.endm
-
-
-/*
     TABLE - creates a table inplace that can be accessed by outside functions. Allocated on the spot
-*/
 
+*/
 
 /*
     Creates an inplace table that can be accessed using the first arguement (string)
@@ -78,8 +100,8 @@
         none
 */
 .macro __TABLE__ name:req, size:req
-.equ __TABLE_\name\(), .
-.org . + ( \size * 4 )
+    .equ __TABLE_\name\(), .
+    .org . + ( \size * 4 )
 .endm
 
 /*
@@ -91,7 +113,7 @@
     OUTPUTS:
         r8 = address of table value
 */
-.macro __TABLE_AT__ name:req, index:vararg
+.macro __TABLE_AT__ name:req, index
     .ifndef __TABLE_\name       // throws error on undefined table
         .print "\n\n !!! '__TABLE_AT__' macro failed as requested table is undefined !!! \n\n"
         .err
@@ -114,7 +136,7 @@
     OUTPUTS:
         none
 */
-.macro __TABLE_SET__ name:req, index:vararg
+.macro __TABLE_SET__ name:req, index
     .ifb \index
         __TABLE_AT__ \name
         str r5, [r8]
@@ -132,7 +154,162 @@
     OUTPUTS:
         r8 = inplace table value at index
 */
-.macro __TABLE_GET__ name:req
-__TABLE_AT__ \name
-ldr r8, [r8]
+.macro __TABLE_GET__ name:req, index
+    .ifb \index
+        __TABLE_AT__ \name
+        ldr r8, [r8]
+    .else
+        __TABLE_AT__ \name, \index
+        ldr r8, [r8]
+    .endif
 .endm
+
+
+
+/*
+
+    HEAP - allows for allocation of data only accesible using a direct index value
+
+*/
+
+// Creates a new heap. This is for all intents and purposes a local heap, but could be used to create a global one. Each heap entry has an additional 1st word denoting size in bytes
+// Inputs:
+//      (macro_arg) heap_name = name of this heap for accessing purposes
+// Outputs:
+//      (macro_arg) size = size of the heap in blocks of 32  4 byte wide words. ie: size = 1 allocates 128 bytes of space
+.macro __HEAP__ heap_name:req, size:req
+    __HEAP_\heap_name\(): .word . + 0x4              // label for referencing the start of the heap
+    .org . + (\size * 128)
+.endm
+
+// Allocates memory on the heap and returns pointer to start of range
+// Inputs:
+//      (macro_arg) heap_name = name of the heap to allocate memory on
+//      (macro_arg) words = width of allocation in words
+// Outputs:
+//      r8 = address of start of newly allocated memory
+.macro __HEAP_ALLOCATE__ heap_name:req, words:req
+
+    // Prepare for heap search
+    ldr r8, __HEAP_\heap_name\()
+    mov r0, #0x0
+
+    // Loop through heap until enough space is found for the allocatation request.
+    __HEAP_ALLOCATE_SEARCH_LOOPBACK_\@:
+
+    // Grab the byte and see if it holds a size value
+    ldr r1, [r8]
+    cmp r1, #0x0
+
+    // If this holds a size, reset the r2 counter and increment the r8 address pointer by size
+    movne r0, #0x0
+    addne r8, r8, r1
+    bne __HEAP_ALLOCATE_SEARCH_LOOPBACK_\@
+
+    // If this is an empty word, increment the r0 counter, and check if we have found enough space yet
+    add r0, r0, #0x4
+    add r8, r8, #0x4
+    cmp r0, #((\words + 1) * 4)
+
+    // If we haven't found enough space, keep looping
+    bmi __HEAP_ALLOCATE_SEARCH_LOOPBACK_\@
+
+    // We have found enough space, set r8 to 0th address
+    sub r8, r8, r0
+    
+    // Set size value and post increment r8 heap address pointer to return to correct address
+    mov r1, #(\words * 4)
+    str r1, [r8], #0x4
+
+.endm
+
+/*
+    Deallocates some memory off of the heap
+    INPUTS:
+        r4 = start address of heap chunk (this is returned from __HEAP_ALLOCATE__ in r8)
+        (macro_arg) heap_name = name of the heap this is referencing
+        (macro_arg) words = number of words including the first referenced by r4 to clear
+    OUTPUTS:
+        none
+*/
+.macro __HEAP_FREE__ heap_name:req, words:req
+    mov r0, r4
+    mov r1, #0x0
+    str r1, [r0, #-0x4]
+    .rept \words
+        str r1, [r0], #0x4
+    .endr
+.endm
+
+
+/*
+    Helper functions!
+*/
+
+
+/*
+    POPs some values off of a stack and allocates heap space for these values. Orignal SP value is preserved. ! values in stack are in last (POP to retrieve last) to first order !
+    INPUTS:
+        (macro_arg) stack_name = name of the stack this is referencing
+        (macro_arg) heap_name = name of the heap this is referencing
+        (macro_arg) words = number of words to pop from the stack and give to the heap
+    OUTPUTS:
+        r8 = first address of allocated heap space
+*/
+.macro __STACK_TO_HEAP__ stack_name:req, heap_name:req, words:req
+    __STACK_SWAP_SP__ \stack_name
+    __HEAP_ALLOCATE__ \heap_name , \words
+    add r8, r8, #(\words * 0x4)
+    .rept (\words\() / 4)
+        __STACK_QPOP__ \stack_name , r0, r1, r2, r3
+        stmdb r8!, {r0, r1, r2, r3}
+    .endr
+    .if (\words\() % 4) == 3
+        __STACK_QPOP__ \stack_name , r0, r1, r2
+        stmdb r8!, {r0, r1, r2}
+    .elseif (\words\() % 4) == 2
+        __STACK_QPOP__ \stack_name , r0, r1
+        stmdb r8!, {r0, r1}
+    .elseif (\words\() % 4) == 1
+        __STACK_QPOP__ \stack_name , r0
+        str r0, [r8, #-0x4]!
+    .endif
+    __STACK_SWAP_SP__ \stack_name
+.endm
+
+/*
+    PUSHs some values onto a stack as a copy of values from within a heap. Original SP value is preserved. ! values in stack are in last (POP to retrieve last) to first order !
+    INPUTS:
+        r4 = first address of region of heap to copy. Same as value returned by any heap allocating macro.
+        (macro_arg) stack_name = name of the stack this is referencing
+        (macro_arg) words = number of words to pop from the stack and give to the heap
+    OUTPUTS:
+        none
+*/
+.macro __HEAP_TO_STACK_COPY__ stack_name:req, words:req
+    __STACK_SWAP_SP__ \stack_name
+    mov r0, r4
+    .rept (\words / 3)
+        ldmia r0!, {r1, r2, r3}
+        __STACK_QPUSH__ \stack_name , r1, r2, r3
+    .endr
+    .if (\words % 3) == 2
+        ldmia r0!, {r1, r2}
+        __STACK_QPUSH__ \stack_nane , r1, r2
+    .elseif (\words % 3) == 1
+        ldr r1, [r0], #0x4
+        __STACK_QPUSH__ \stack_name , r1
+    .endif
+    __STACK_SWAP_SP__ \stack_name
+.endm
+
+/*
+    Same as __HEAP_TO_STACK_COPY__ but free's the memory from the heap after the transfer ! values in stack are in last (POP to retrieve last) to first order !
+    ADDITIONAL INPUTS:
+        (macro_arg) heap_name = name of the heap to free memory from
+*/
+.macro __HEAP_TO_STACK_MOVE__ heap_name:req, stack_name:req, words:req
+    __HEAP_TO_STACK_COPY__ \stack_name , \words
+    __HEAP_FREE__ \heap_name , \words
+.endm
+
--- modules/kernel/kernel7/kernel7.s
+++ modules/kernel/kernel7/kernel7.s
@@ -9,6 +9,9 @@
 .arm
 .align
 
+// Include our utilities toolkit
+.include "../../asm_utils.s"
+
 
 /*
     Major func jump address table
@@ -83,8 +86,9 @@ KR_KERNEL_FRQ:
         add r3, r3, #0x8
     */
 
-K_GPIO_ADDRESS: .word GPIO_DRIVER
-K_UART_ADDRESS: .word UART_DRIVER
+
+// Create a table for storing address of loaded drivers
+__TABLE__ "drivers", 12
 
 
 // Macro for calling a driver subroutine, r0 should be the driver start address
@@ -95,61 +99,103 @@ K_UART_ADDRESS: .word UART_DRIVER
     mov pc, r0
 .endm
 
-.include "../../asm_utils.s"
+
+// Create a table for testing
 __TABLE__ "test_table", 12
+
+// Create a stack for testing
+__STACK__ "test_stack", 8
+
+// Create a heap for testing
+__HEAP__ "test_heap", 2
 
 // Code enters here, run the boot load stage
 startup_:
 
-    // Try the __TABLE_AT__ macro using a runtime address evaluation
-    mov r4, #4
-    __TABLE_AT__ "test_table"
+    // Try the __TABLE_SET__ macro using a runtime address evaluation
+    TABLE_SET_TEST:
+        mov r4, #4
+        mov r5, #0x2C
+        __TABLE_SET__ "test_table"
     
-    // Try the __TABLE_AT__ macro using a constexpr address evaluation
-    __TABLE_AT__ "test_table", 4
+    // Try the __TABLE_SET__ macro using a constexpr address evaluation
+    TABLE_CONSTEXPR_SET_TEST:
+        mov r5, #0x59
+        __TABLE_SET__ "test_table", 4
+
+    STACK_SP_SWAP_TEST:
+        __STACK_SWAP_SP__ "test_stack"
+    
+    STACK_QPUSH_TEST:
+        mov r0, #0xA
+        mov r1, #0x5
+        mov r7, #0xCB2
+        __STACK_QPUSH__ "test_stack", r0, r1, r7
+
+    STACK_QPOP_TEST:
+        __STACK_QPOP__ "test_stack", r4, r5, r6
+
+    STACK_SP_UNSWAP_TEST:
+        __STACK_SWAP_SP__ "test_stack"
+
+    STACK_PUSH_TEST:
+        mov r0, #0xA
+        mov r1, #0x5
+        mov r7, #0xCB2
+        __STACK_PUSH__ "test_stack", r0, r1, r7
+
+    STACK_TO_HEAP_TEST:
+        __STACK_TO_HEAP__ "test_stack", "test_heap", 3
+
+    HEAP_TO_STACK_MOVE_TEST:
+        __HEAP_TO_STACK_MOVE__ "test_heap", "test_stack", 3
+        
 
 
-
-    // Test out the table
-    mov r4, #0
-    mov r5, #0x2C
+    END_OF_UTILS_TESTING:
 
     // Register GPIO driver
-    ldr r0, =GPIO_DRIVER        
-    str r0, K_GPIO_ADDRESS
+    ldr r4, GPIO_DRIVER
+    __TABLE_SET__ "drivers", 0
     ldr r1, =0x3F200000
-    str r1, [r0, #8]
+    str r1, [r4, #8]
 
     // Register UART driver
-    ldr r0, =UART_DRIVER        
-    str r0, K_UART_ADDRESS
+    ldr r4, UART_DRIVER
+    __TABLE_SET__ "drivers", 1
     ldr r1, =0x3F201000
-    str r1, [r0, #8]
+    str r1, [r4, #8]
 
     // Set ACT led to output mode
-    ldr r0, K_GPIO_ADDRESS
+    __TABLE_GET__ "drivers", 0
+    mov r0, r4
     mov r5, #29
     __D_CALL__ 8, 12
 
     // Set ACT led to high to turn on the indicator LED
-    ldr r0, K_GPIO_ADDRESS
+    __TABLE_GET__ "drivers", 0
+    mov r0, r4
     mov r5, #29
     __D_CALL__ 0, 12
 
     // Set pin 14 and pin 15 to mode alternate function 0 (enables UART on those pins)
-    ldr r0, K_GPIO_ADDRESS
+    __TABLE_GET__ "drivers", 0
+    mov r0, r4
     mov r5, #14
     __D_CALL__ 9, 12
-    ldr r0, K_GPIO_ADDRESS
+    __TABLE_GET__ "drivers", 0
+    mov r0, r4
     mov r5, #15
     __D_CALL__ 9, 12
 
     // Enable the UART driver
-    ldr r0, K_UART_ADDRESS
+    __TABLE_GET__ "drivers", 1
+    mov r0, r4
     __D_CALL__ 0, 12
 
     // Try and send across a single byte = (0x4C)
-    ldr r0, K_UART_ADDRESS
+    __TABLE_GET__ "drivers", 1
+    mov r0, r4
     mov r4, #0x4C
     __D_CALL__ 3, 12
 
@@ -164,17 +210,15 @@ final_:
 
 
 // GPIO driver temp direct include
-GPIO_DRIVER:
 .align
+GPIO_DRIVER: .word .
 .incbin "../../../build/drivers/0e_gpio/0e1000-32-gpio.bin"
 
 // UART driver temp direct include
-UART_DRIVER:
 .align
+UART_DRIVER: .word .
 .incbin "../../../build/drivers/0m_uart/0m1000-32-uart.bin"
 
-
-
+.align
 
 DRIVER_INCLUDES_END:
-.align
